a0d9befb605bc7c43c754669c48ede22d0e9b178
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2023-03-22 01:35:35 -0500

    feat: ulid sqlx type
    This introduces a Ulid struct which implements
    sqlx::Type<sqlx::Postgres>, meaning it can be decoding from queries.
    Support in sqlx for these custom types seems not entirely fleshed out,
    but with some extra code at query time this works fine.

--- crates/models/src/lib.rs
+++ crates/models/src/lib.rs
@@ -1,7 +1,17 @@
+use lockpad_ulid::Ulid;
+use sqlx::types::Uuid;
+
 pub mod application;
 pub mod entity;
 pub mod error;
 pub mod user;
+
+#[derive(Debug)]
+pub struct User {
+    pub user_id: Option<lockpad_ulid::Ulid>,
+    // pub user_id: Option<sqlx::types::Uuid>,
+    pub name: String,
+}
 
 pub async fn temp_example() {
     use sqlx::postgres::PgPool;
@@ -11,8 +21,48 @@ pub async fn temp_example() {
         .await
         .unwrap();
 
-    let _query = sqlx::query!("SELECT name FROM users")
-        .fetch_one(&pool)
+    // let query = sqlx::query!(r#"SELECT user_id::TEXT as "user_id: Ulid" FROM users"#)
+    // let query = sqlx::query!(r#"SELECT user_id::UUID as "user_id: Uuid" FROM users"#)
+
+    let insert = sqlx::query!(
+        r#"INSERT INTO users (name, secret) VALUES ($1, $2)"#,
+        "test",
+        "secret"
+    )
+    .execute(&pool)
+    .await
+    .unwrap();
+
+    let generated_id = Ulid::generate();
+    let generated_uuid = Uuid::from(generated_id);
+    let generated_string = generated_id.to_string();
+    let uuid_string = generated_uuid.to_string();
+    let second_insert = sqlx::query!(
+        r#"
+        INSERT INTO 
+            users(user_id, name, secret)
+        SELECT user_id::uuid, name, secret
+        FROM(
+            VALUES($1, $2, $3)
+        ) AS data(user_id, name, secret)
+        "#,
+        uuid_string,
+        "generated-test",
+        "secret"
+    )
+    .execute(&pool)
+    .await
+    .unwrap();
+
+    let query = sqlx::query!(r#"SELECT user_id::UUID as "user_id: Ulid" FROM users"#)
+        // let query = sqlx::query_as!(User, r#"SELECT name, user_id::UUID as "user_id: Ulid" FROM users"#)
+        .fetch_all(&pool)
         .await
         .unwrap();
+
+    query.iter().for_each(|user| {
+        println!("{:?}", user);
+        let id = user.user_id.unwrap();
+        println!("{:?}", id);
+    });
 }
