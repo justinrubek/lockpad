9a0b99399f61760472e6e007b18e1c48983462c6
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2023-04-02 16:02:27 -0500

    feat: added HtmlPage struct
    This allows for sharing common html values across pages without having
    to call out to an external templating system. This removes all duplicate
    styles in favor of a unified one. The login and signup forms shared
    nearly the exact same code and the redundancy has been eliminated.

--- crates/http/src/handlers/pages.rs
+++ crates/http/src/handlers/pages.rs
@@ -1,142 +1,104 @@
-pub(crate) async fn root() -> axum::response::Html<&'static str> {
-    axum::response::Html(
-        r#"
-        <html>
-            <head>
-                <title>Lockpad</title>
-            </head>
-            <body>
-                <h1>Hello, World!</h1>
-            </body>
+use std::str::FromStr;
 
-            <a href="/login">Login</a>
-            <a href="/signup-screen">Signup</a>
-        </html>
-    "#,
-    )
+use axum::{
+    extract::{Query, State},
+    response::IntoResponse,
+};
+use lockpad_models::application::Application;
+use lockpad_ulid::Ulid;
+use serde::Deserialize;
+
+use crate::ServerState;
+
+pub(crate) async fn root() -> impl IntoResponse {
+    HtmlPage::Default
+}
+
+#[derive(Debug, Deserialize)]
+pub(crate) struct LoginScreenQuery {
+    pub redirect_uri: String,
+    pub client_id: String,
 }
 
 /// Sends a screen that asks the user to provide credentials.
-pub(crate) async fn login_screen() -> axum::response::Html<&'static str> {
-    axum::response::Html(
-        r#"
-        <h1>log in</h1>
-        <form id="login-form">
-            <input type="text" id="username" name="username" placeholder="username" />
-            <input type="password" id="password" name="password" placeholder="password" />
-            <input type="submit" value="Login" />
-        </form>
+pub(crate) async fn login_screen(
+    query: Option<Query<LoginScreenQuery>>,
+    State(ServerState { pg_pool, .. }): State<ServerState>,
+) -> impl IntoResponse {
+    let params = match query {
+        None => return HtmlPage::NoParams,
+        Some(query) => query.0,
+    };
+    tracing::info!("login screen query: {:?}", params);
 
-        <script>
-            const form = document.getElementById("login-form");
-            form.onsubmit = function(event) {
-                console.log("submitting form");
-                event.preventDefault();
-                const data = new FormData(form);
-                const username = data.get("username");
-                const password = data.get("password");
+    // TODO: Lookup the client_id as the application_id and determine if the redirect_uri is valid.
+    // If the client_id is not valid, return an error.
+    // If the redirect_uri is not valid, return an error.
+    let app_id = Ulid::from_str(&params.client_id).unwrap();
+    let application = match Application::by_id(&pg_pool, &app_id).await {
+        Err(_) => return HtmlPage::InvalidParams,
+        Ok(None) => return HtmlPage::InvalidParams,
+        Ok(Some(application)) => application,
+    };
+    tracing::info!("application: {:?}", application);
 
-                // Perform a POST request to /authorize
-                // If the request is successful, the s
-                fetch("/authorize", {
-                    method: "POST",
-                    body: JSON.stringify({ username, password }),
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                })
-                .then(response => response.json())
-                .then(data => {
-                    console.log("Success:", data);
-                    // navigate to the main page
-                    // TODO: this value should be populated from the server
-                    window.location.href = "/";
-                })
-                .catch((error) => {
-                    console.error("Error:", error);
-                });
-            }
-        </script>
-
-        <style>
-            form {
-                display: flex;
-                flex-direction: column;
-                align-items: center;
-            }
-
-            input {
-                margin: 0.5rem;
-            }
-
-            input[type="submit"] {
-                width: 100px;
-            }
-
-            input[type="text"], input[type="password"] {
-                width: 200px;
-            }
-
-            input[type="text"]:focus, input[type="password"]:focus {
-                outline: none;
-            }
-
-            h1 {
-                text-align: center;
-            }
-        </style>
-    "#,
-    )
+    HtmlPage::Form(HtmlForm::login("/authorize".to_string()))
 }
 
 /// Sends a screen that asks the user to provide credentials.
 /// These credentials will be used to create a new account.
 /// This closely follows the login screen.
-pub(crate) async fn signup_screen() -> axum::response::Html<&'static str> {
+pub(crate) async fn signup_screen() -> impl IntoResponse {
     // Keep this really simple for now.
     // Later this could be its own dedicated page, but for now it's just a simple form.
+    HtmlPage::Form(HtmlForm::register("/signup".to_string()))
+}
 
-    axum::response::Html(
-        r#"
-        <h1>sign up</h1>
-        <form id="signup-form">
-            <input type="text" id="username" name="username" placeholder="username" />
-            <input type="password" id="password" name="password" placeholder="password" />
-            <input type="submit" value="Sign up" />
-        </form>
+/// A response that sends an HTML page
+enum HtmlPage {
+    /// There was no origin provided in the request.
+    #[allow(dead_code)]
+    NoOrigin,
+    /// There were no application parameters provided in the request.
+    NoParams,
+    /// The application parameters were invalid.
+    InvalidParams,
+    /// display a form
+    Form(HtmlForm),
+    /// The default page
+    Default,
+}
 
-        <script>
-            const form = document.getElementById("signup-form");
-            form.onsubmit = function(event) {
-                console.log("submitting form");
-                event.preventDefault();
-                const data = new FormData(form);
-                const username = data.get("username");
-                const password = data.get("password");
-
-                // Perform a POST request to /signup
-                // If the request is successful, the s
-                fetch("/signup", {
-                    method: "POST",
-                    body: JSON.stringify({ username, password }),
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                })
-                .then(response => response.json())
-                .then(data => {
-                    console.log("Success:", data);
-                    // navigate to the main page
-                    window.location.href = "/";
-                })
-                .catch((error) => {
-                    console.error("Error:", error);
-                });
-
-            }
-        </script>
-
+impl axum::response::IntoResponse for HtmlPage {
+    fn into_response(self) -> axum::response::Response {
+        let common_html = r#"
         <style>
+            .container {
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+            }
+
+            h1 {
+                text-align: center;
+            }
+
+            p {
+                width: 500px;
+            }
+
+            ul {
+                width: 500px;
+            }
+
+            li {
+                margin: 0.5rem;
+            }
+
+            li strong {
+                font-weight: bold;
+            }
+
             form {
                 display: flex;
                 flex-direction: column;
@@ -158,11 +120,174 @@ pub(crate) async fn signup_screen() -> axum::response::Html<&'static str> {
             input[type="text"]:focus, input[type="password"]:focus {
                 outline: none;
             }
-
-            h1 {
-                text-align: center;
-            }
         </style>
-    "#,
-    )
+    "#;
+
+        let html = match self {
+            HtmlPage::Form(form) => form.to_string(),
+            HtmlPage::NoOrigin => {
+                r#"
+                <div class="container">
+                    <h1>log in</h1>
+                    <p>
+                        If you are seeing this page, it means you have arrived here by accident.
+                        Your request did not include an origin header, which is required for security reasons.
+                    </p>
+                </div>
+            "#.to_string()
+            }
+            HtmlPage::NoParams => {
+                r#"
+                <div class="container">
+                    <h1>log in</h1>
+                    <p>
+                    If you are seeing this page, it means that you have been directed here by an application that is trying to log you in.
+                    However, the application did not provide the necessary parameters to complete the login process.
+                    The application needs to provide the following parameters:
+                    </p>
+                    <ul>
+                        <li><strong>redirect_uri</strong></li>
+                        <li><strong>client_id</strong></li>
+                    </ul>
+                    <p>
+                    Without this information, the login page has no way of knowing where to redirect you after you have logged in.
+                    </p>
+                </div>
+            "#.to_string()
+            }
+            HtmlPage::InvalidParams => {
+                r#"
+                <div class="container">
+                    <h1>log in</h1>
+                    <p>
+                    If you are seeing this page, it means that you have been directed here by an application that is trying to log you in.
+                    However, the application did not provide the necessary parameters to complete the login process.
+                    The application needs to provide the following parameters:
+                    </p>
+                    <ul>
+                        <li><strong>redirect_uri</strong></li>
+                        <li><strong>client_id</strong></li>
+                    </ul>
+                    <p>
+                    Without this information, the login page has no way of knowing where to redirect you after you have logged in.
+                    </p>
+                </div>
+            "#.to_string()
+            }
+            HtmlPage::Default => {
+                r#"
+                    <div class="container">
+                        <h1>lockpad</h1>
+                        <a href="/login">log in</a>
+                        <a href="/signup">sign up</a>
+                    </div>
+                "#.to_string()
+            }
+        };
+
+        axum::response::Html(format!("{}{}", common_html, html)).into_response()
+    }
+}
+
+enum HtmlFormType {
+    /// Allow for user account creation
+    Register,
+    /// Allows for user login
+    Login,
+}
+
+/// Represents the html for <form> + <script> tags
+struct HtmlForm {
+    form_type: HtmlFormType,
+    submit_uri: String,
+}
+
+impl HtmlForm {
+    fn new(form_type: HtmlFormType, submit_uri: String) -> Self {
+        Self {
+            form_type,
+            submit_uri,
+        }
+    }
+
+    /// Build a registeration form
+    fn register(submit_uri: String) -> Self {
+        Self::new(HtmlFormType::Register, submit_uri)
+    }
+
+    /// Build a login form
+    fn login(submit_uri: String) -> Self {
+        Self::new(HtmlFormType::Login, submit_uri)
+    }
+}
+
+impl std::string::ToString for HtmlForm {
+    fn to_string(&self) -> String {
+        let type_name = match self.form_type {
+            HtmlFormType::Register => "register",
+            HtmlFormType::Login => "login",
+        };
+
+        let type_display = match self.form_type {
+            HtmlFormType::Register => "Sign up",
+            HtmlFormType::Login => "Login",
+        };
+
+        let form_name = format!("{}-form", type_name);
+
+        let form_html = format!(
+            r#"
+            <form id="{form_name}">
+                <input type="text" id="username" name="username" placeholder="username" />
+                <input type="password" id="password" name="password" placeholder="password" />
+                <input type="submit" value="{type_display}" />
+            </form>
+        "#,
+        );
+
+        let submit_uri = &self.submit_uri;
+
+        let script_html = format!(
+            r#"
+                <script>
+                    const form = document.getElementById("{form_name}");
+                    form.onsubmit = function(event) {{
+                        console.log("submitting form");
+                        event.preventDefault();
+                        const data = new FormData(form);
+                        const username = data.get("username");
+                        const password = data.get("password");
+
+                        // Perform a POST request to /authorize
+                        // If the request is successful, the s
+                        fetch("{submit_uri}", {{
+                            method: "POST",
+                            body: JSON.stringify({{ username, password }}),
+                            headers: {{
+                                "Content-Type": "application/json",
+                            }},
+                        }})
+                        .then(response => response.json())
+                        .then(data => {{
+                            console.log("Success:", data);
+                        }})
+                        .catch((error) => {{
+                            console.error("Error:", error);
+                        }});
+                    }}
+                </script>
+            "#
+        );
+
+        format!(
+            r#"
+                <h1>{type_display}</h1>
+                {form_html}
+                {script_html}
+            "#,
+            type_display = type_display,
+            form_html = form_html,
+            script_html = script_html,
+        )
+    }
 }
