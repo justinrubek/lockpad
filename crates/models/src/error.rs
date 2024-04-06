c0d8998e0b1da10dc769963a646329e342e99632
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2024-04-06 12:13:48 -0500

    refactor: remove dynamodb

--- crates/models/src/error.rs
+++ crates/models/src/error.rs
@@ -1,24 +1,7 @@
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
     #[error(transparent)]
-    SerdeDynamo(#[from] serde_dynamo::Error),
-    #[error(transparent)]
     SerdeJson(#[from] serde_json::Error),
-
-    #[error(transparent)]
-    DynamodbQuery(#[from] aws_sdk_dynamodb::types::SdkError<aws_sdk_dynamodb::error::QueryError>),
-    #[error(transparent)]
-    DynamodbPut(#[from] aws_sdk_dynamodb::types::SdkError<aws_sdk_dynamodb::error::PutItemError>),
-    #[error(transparent)]
-    DynamodbGet(#[from] aws_sdk_dynamodb::types::SdkError<aws_sdk_dynamodb::error::GetItemError>),
-    #[error(transparent)]
-    DynamodbDelete(
-        #[from] aws_sdk_dynamodb::types::SdkError<aws_sdk_dynamodb::error::DeleteItemError>,
-    ),
-    #[error(transparent)]
-    DynamodbUpdate(
-        #[from] aws_sdk_dynamodb::types::SdkError<aws_sdk_dynamodb::error::UpdateItemError>,
-    ),
 
     #[error(transparent)]
     Sqlx(#[from] sqlx::Error),
